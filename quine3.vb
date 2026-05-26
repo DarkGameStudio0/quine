2414a00ead5cc4dbd3dd95293bd740bfd1ae755d
Master TypeScript C++ C# <3476647793@qq.com>
2026-05-27 00:04:04 +0800

    Create quine3.vb

---
+++ quine3.vb
@@ -0,0 +1,8 @@
+Module Quine
+    Sub Main()
+        Dim s As String = "Module Quine%Sub Main()%Dim s As String = {0}{1}{0}%s = s.Replace({0}{2}{0}, {0}{0}{0})%s = s.Replace({0}{3}{0}, {0}{1}{0})%Console.WriteLine(s, Chr(34), s, Chr(34) & vbCrLf & Chr(34))%End Sub%End Module"
+        s = s.Replace(Chr(34) & vbCrLf & Chr(34), Chr(34) & Chr(34) & Chr(34))
+        s = s.Replace(Chr(34) & "=" & Chr(34), Chr(34) & "&" & Chr(34))
+        Console.WriteLine(s, Chr(34), s, Chr(34) & vbCrLf & Chr(34))
+    End Sub
+End Module
